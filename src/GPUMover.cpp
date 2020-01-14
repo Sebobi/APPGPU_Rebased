c207489d60ec88e792cf8b51c03fd597788206ea
Sebobi <badlocationexception@gmail.com>
2020-01-14 03:08:49 +0100

    meme

--- src/GPUMover.cpp
+++ src/GPUMover.cpp
@@ -27,7 +27,13 @@ void copyGrid(grid *cpu_grid, grid *gpu_grid, size_t size){
 		std::cout << "CUDA FAILED MALLOC3" << std::endl;
 	}
 	code = cudaMemcpy(XN_flat, cpu_grid->XN_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
+	if (code != cudaSuccess) {
+		std::cout << "CUDA FAILED MALLOC4" << std::endl;
+	}
 	code = cudaMemcpy(&(gpu_grid->XN_flat), &XN_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
+	if (code != cudaSuccess) {
+		std::cout << "CUDA FAILED MALLOC5" << std::endl;
+	}
 
 	code = cudaMalloc(&YN_flat, size * sizeof(FPfield));
 	code = cudaMemcpy(YN_flat, cpu_grid->YN_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
@@ -57,6 +63,11 @@ void copyParam(parameters *param, parameters *gpu_param) {
 
 void copyEMfield(EMfield *emf, EMfield *gpu_emf, size_t size) {
 
+	cudaError_t code;
+
+
+
+
 
 	FPfield* Ex_flat;
 	FPfield* Ey_flat;
@@ -65,34 +76,49 @@ void copyEMfield(EMfield *emf, EMfield *gpu_emf, size_t size) {
 	FPfield* Byn_flat;
 	FPfield* Bzn_flat;
 
-	cudaMalloc(&gpu_emf, sizeof(EMfield));
-	cudaMemcpy(gpu_emf, emf, sizeof(EMfield), cudaMemcpyHostToDevice);
+	code = cudaMalloc(&gpu_emf, sizeof(EMfield));
+	if (code != cudaSuccess) {
+		std::cout << "CUDA FAILED MALLOC1" << std::endl;
+	}
+	code = cudaMemcpy(gpu_emf, emf, sizeof(EMfield), cudaMemcpyHostToDevice);
+	if (code != cudaSuccess) {
+		std::cout << "CUDA FAILED MALLOC2" << std::endl;
+	}
 
 
 
 	//Ex
 	//malloc space for pointer(array)
-	cudaMalloc(&Ex_flat, size * sizeof(FPfield));
+	code = cudaMalloc(&Ex_flat, size * sizeof(FPfield));
+	if (code != cudaSuccess) {
+		std::cout << "CUDA FAILED MALLOC3" << std::endl;
+	}
 	//fill array with values
-	cudaMemcpy(Ex_flat, emf->Ex_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMemcpy(Ex_flat, emf->Ex_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
+	if (code != cudaSuccess) {
+		std::cout << "CUDA FAILED MALLOC4" << std::endl;
+	}
 	//make our gpu_emf->ex_flat point to the address of ex_flat we just created.
-	cudaMemcpy(&(gpu_emf->Ex_flat), &Ex_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMemcpy(&(gpu_emf->Ex_flat), &Ex_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
+	if (code != cudaSuccess) {
+		std::cout << "CUDA FAILED MALLOC5" << std::endl;
+	}
 	//ey
-	cudaMalloc(&Ey_flat, size * sizeof(FPfield));
-	cudaMemcpy(Ey_flat, emf->Ey_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
-	cudaMemcpy(&(gpu_emf->Ey_flat), &Ey_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMalloc(&Ey_flat, size * sizeof(FPfield));
+	code = cudaMemcpy(Ey_flat, emf->Ey_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMemcpy(&(gpu_emf->Ey_flat), &Ey_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
 	//ez
-	cudaMalloc(&Ez_flat, size * sizeof(FPfield));
-	cudaMemcpy(Ez_flat, emf->Ez_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
-	cudaMemcpy(&(gpu_emf->Ez_flat), &Ez_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMalloc(&Ez_flat, size * sizeof(FPfield));
+	code = cudaMemcpy(Ez_flat, emf->Ez_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMemcpy(&(gpu_emf->Ez_flat), &Ez_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
 	//bx
-	cudaMalloc(&Bxn_flat, size * sizeof(FPfield));
-	cudaMemcpy(Bxn_flat, emf->Bxn_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
-	cudaMemcpy(&(gpu_emf->Bxn_flat), &Bxn_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMalloc(&Bxn_flat, size * sizeof(FPfield));
+	code = cudaMemcpy(Bxn_flat, emf->Bxn_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMemcpy(&(gpu_emf->Bxn_flat), &Bxn_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
 	//by
-	cudaMalloc(&Byn_flat, size * sizeof(FPfield));
-	cudaMemcpy(Byn_flat, emf->Byn_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
-	cudaMemcpy(&(gpu_emf->Byn_flat), &Byn_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMalloc(&Byn_flat, size * sizeof(FPfield));
+	code = cudaMemcpy(Byn_flat, emf->Byn_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
+	code = cudaMemcpy(&(gpu_emf->Byn_flat), &Byn_flat, sizeof(FPfield), cudaMemcpyHostToDevice);
 	//bz
 	cudaMalloc(&Bzn_flat, size * sizeof(FPfield));
 	cudaMemcpy(Bzn_flat, emf->Bzn_flat, size * sizeof(FPfield), cudaMemcpyHostToDevice);
